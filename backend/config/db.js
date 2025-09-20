0679cd072b847f2f78477b1cdbe002beb0dd2da4
sayaligurav7789 <sayligurab7789@gmail.com>
2025-09-20 22:35:46 +0530

    Added db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,17 @@ const pg = require('pg');
 
 require('dotenv').config();
 
+
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
+  ssl: process.env.DATABASE_SSL === "true" 
+    ? { rejectUnauthorized: false } 
     : false,
 });
+
+pool.connect()
+  .then(() => console.log("Connected to Postgres"))
+  .catch(err => console.error("Postgres connection error", err.stack));
 
 module.exports = pool;
